70ed44db279b7afd30c4fd5b935d8ff8beb56fc5
oscar Velez <ovelez_24@hotmail.com>
2022-06-28 09:36:24 -0500

    primercambio

---
+++ java/Account.java
@@ -0,0 +1,7 @@
+class Account {
+    Integer id;
+    String name;
+    String documents;
+    String email;
+    String password;
+}
---
+++ java/Main.java
@@ -0,0 +1,7 @@
+class Main {
+    Run | Debug
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+    }
+}
+
